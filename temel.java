4b31fc4c77382a0f16266a15ffb0089d9d795b97
Seyma <63148309+lacilobaid@users.noreply.github.com>
2021-06-23 20:57:53 +0300

    Update temel.java

--- temel.java
+++ temel.java
@@ -4,7 +4,7 @@ public class b {
      }
 }
 
-// ----------------------------------------------
+----------------------------------------------
 public class b { 
  public static void main(String[] args){
   int a=5;
@@ -14,7 +14,7 @@ public class b {
   System.out.println("toplam=" + c); //yorum /* çoklu yorum */
   }
 }
-// ----------------------------------------------
+----------------------------------------------
 public class b{
  public static void main(String[] args){
     // int tam sayı 
@@ -24,14 +24,14 @@ public class b{
     // byte --> short --> int --> long
     }
 }
-// ------------------------------------------------
+------------------------------------------------
 public class b{
   public static void main(String[] args){
         // double 64bit ondalıklı sayı
         // float a=2.5f;  32bit
   }
 }
-// -----------------------------------------------
+-----------------------------------------------
 public class b{ 
   public static void main(String[] args){
       // char: karakter göstermek için. 2^16 tane karakter simgeler. 2 byte yer kaplar. unicoode number. 
@@ -39,13 +39,13 @@ public class b{
        
   }
 }
-// -------------------------------------------------
+-------------------------------------------------
 public class b{
   public static void main(String[] args){
       // String, ilkel olmayan veri tipi.
   }
 }
-// ----------------------------------------------------
+----------------------------------------------------
 public class b{
   public static void main(String[] args){
     Scanner scanner=new Scanner(System.in);
@@ -60,7 +60,7 @@ public class b{
     } else{...}
   }
 }
-// -----------------------------------------------------
+-----------------------------------------------------
 public class b{
   public static void main(String[] args){
    Scanner scanner=new Scanner(System.in);
@@ -70,24 +70,24 @@ public class b{
       // intten sonra line istenildiği zaman hata verir.
  }
 }
-// ---------------------------------------------------------
+---------------------------------------------------------
 // Beden kitle indeksi hesaplama.
   public class b{
    public static void main(Sting[] args){ 
 
-     Scanner boyKilo=new Scanner(System.in);
+     Scanner scanner=new Scanner(System.in);
 
       System.out.print("Boy girin:");
-        double boy = boyKilo.nextDouble();
+        double boy = scanner.nextDouble();
       System.out.print("Kilo girin:");
-        double kilo = boyKilo.nextDouble();
+        double kilo = scanner.nextDouble();
 
        double indeks=(kilo/(boy*boy))
 
      System.out.print("toplam=" + indeks);
  }
 }
-// -------------------------------------------------------------
+-------------------------------------------------------------
 // Kilometre basına tutar hesaplama.
 public class b{
  public static void main(String[] args){
@@ -120,7 +120,7 @@ public class b{
     System.out.print("Hipotenüs:" + hipo);
  }
 }
-// ------------------------------------------------------------
+------------------------------------------------------------
 // Mantiksal Operator.
  public class b{
   public static void main(String[] args){
@@ -129,19 +129,19 @@ public class b{
   // ! --> NOT operatoru
  }
 }
-// -------------------------------------------------------------
+-------------------------------------------------------------
 // Kullanıcıdan alınan 3 sayıdan en büyüğünü bulma.
  public class b{
   public static void main (String[] args){
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
       
     int a, b, c;
      System.out.print("a değeri girin:");
-        a=sc.nextInt();
+        a=scanner.nextInt();
      System.out.print("b değeri girin:");
-        b=sc.nextInt();
+        b=scanner.nextInt();
      System.out.print("c değeri girin:");
-        c=sc.nextInt();
+        c=scanner.nextInt();
 
      int maks=-1;
 
@@ -156,17 +156,17 @@ public class b{
          System.out.print("En büyük sayı:" +maks);
  }
 }
-// ------------------------------------------------------
+------------------------------------------------------
 // Koşullarla beden kitle indeksi hesaplama.
 public class b{
    public static void main(Sting[] args){ 
 
-     Scanner boyKilo=new Scanner(System.in);
+     Scanner scanner=new Scanner(System.in);
 
       System.out.print("Boy girin:");
-        double boy = boyKilo.nextDouble();
+        double boy = scanner.nextDouble();
       System.out.print("Kilo girin:");
-        double kilo = boyKilo.nextDouble();
+        double kilo = scanner.nextDouble();
 
        double indeks=(kilo/(boy*boy));
 
@@ -184,12 +184,12 @@ public class b{
     }
   }
 }
-// -----------------------------------------------------------------
+-----------------------------------------------------------------
 // Basit hesap makinesi.
  public class b{
   public static void main(String[] args){
     int a,b ;
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     String İslemler="1. Toplama İşlemi\n"
                    +"2. Çıkarma İşlemi\n"
@@ -197,35 +197,35 @@ public class b{
                    +"4. Bölme İşlemi\n";
      
    System.out.println("İşlem seçin:");
-   String islem = sc.nextLine();
+   String islem = scanner.nextLine();
       
    switch(islem){
     case "1" ;
        System.out.print("Birinci sayı:");
-         a=sc.nextInt();
+         a=scanner.nextInt();
        System.out.print("İkinci sayı:");
-         b=sc.nextInt();
+         b=scanner.nextInt();
        System.out.println("Toplam:" +(a+b)); 
         break;
     case "2" ;
        System.out.print("Birinci sayı:");
-         a=sc.nextInt();
+         a=scanner.nextInt();
        System.out.print("İkinci sayı:");
-         b=sc.nextInt();
+         b=scanner.nextInt();
        System.out.println("Fark:" +(a-b)); 
         break;
     case "3" ;
        System.out.print("Birinci sayı:");
-         a=sc.nextInt();
+         a=scanner.nextInt();
        System.out.print("İkinci sayı:");
-         b=sc.nextInt();
+         b=scanner.nextInt();
        System.out.println("Çarpım:" +(a*b)); 
         break;
     case "4" ;
        System.out.print("Birinci sayı:");
-         a=sc.nextInt();
+         a=scanner.nextInt();
        System.out.print("İkinci sayı:");
-         b=sc.nextInt();
+         b=scanner.nextInt();
        System.out.println("Toplam:" +((double)a/b)); 
         break;
     default;
@@ -233,21 +233,21 @@ public class b{
   }
  }
 }
-// ------------------------------------------------------------
+------------------------------------------------------------
 // Gelismis not hesaplama.
  public class b{
   public static void main(String[] args){
 
     int vize, vize2, final;
-    Scanner sc=new Scanner(System.in);
+    Scanner scanner=new Scanner(System.in);
       System.out.print("Vize girin:");
-        int vize = sc.nextInt();
+        int vize = scanner.nextInt();
       System.out.print("Vize2 girin:");
-        int vize2 = sc.nextInt();
+        int vize2 = scanner.nextInt();
       System.out.print("Final girin:");
-        int final=sc.nextInt();
+        int final=scanner.nextInt();
       System.out.print("Okul ortalamanızı girin:");
-        double ortalama=sc.nextDouble(); 
+        double ortalama=scanner.nextDouble(); 
 
     double not=(vize*0.30)+(vize2*0.30)+(final*0.40);
     
@@ -265,13 +265,13 @@ public class b{
        else{System.out.print("FF");}
  } 
 }
-// ------------------------------------------------------------------
+------------------------------------------------------------------
 public class b{ 
  public static void main(String[] args){ 
       // for(baslatma;kosul;arttırma veya azaltma)
       // faktoriyel hesaplama.
 
-   Scanner sc=new Scanner(System.in);
+   Scanner scanner=new Scanner(System.in);
       System.out.print("Sayi girin:");
        int sayi=scanner.nextInt();
 
@@ -284,13 +284,13 @@ public class b{
      
  }
 }
-// ---------------------------------------------------------------
+---------------------------------------------------------------
 public class b{
  public static void main(String[] args){
     // while(koşul)
     // faktoriyel hesaplama.
 
-   Scanner sc=new Scanner(System.in);
+   Scanner scanner=new Scanner(System.in);
       System.out.print("Sayi girin:");
       int sayi=scanner.nextInt();
 
@@ -305,13 +305,13 @@ public class b{
       System.out.print("Faktoriyel="+fakt);
  }
 }
-// ---------------------------------------------------------------
+---------------------------------------------------------------
 public class b{
   public static void man(String[] args)
     /* do{ 
          }while(koşul); */ // mutlaka bir kere çalışır.
 
-      Scanner sc = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
         System.out.print("Sayi girin:");
         int sayi=scanner.nextInt();
         int toplam=0;
@@ -323,7 +323,7 @@ public class b{
      
  }
 }
-// -----------------------------------------------------------------
+-----------------------------------------------------------------
 public class b{
   public static void main(String[] args)
     // break: döngüyü sonlandırır. sadece içinde bulundugu döngüde sonlandırır.
@@ -331,12 +331,12 @@ public class b{
 
  }
 }
-// -------------------------------------------------------------------
+-------------------------------------------------------------------
 // ATM örneği.
 public class b{
   public static void main(String[] args)
     int bakiye = 1000;
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     String İslemler="1. Bakiye Ogrenme \n"
                    +"2. Para Cekme \n"
@@ -345,7 +345,7 @@ public class b{
                    
    while(true){
      System.out.println("İşlem seçin:");
-     String islem = sc.nextLine();
+     String islem = scanner.nextLine();
 
        if(islem.equals("q")){
           System.out.print("Programdan cıkılıyor...");
@@ -357,7 +357,7 @@ public class b{
        else if(islem.equals("2")){
          System.out.print("Cekmek istenilen tutar: ");
          int tutar=scanner.nextInt();
-         sc.nextLine();
+         scanner.nextLine();
 
           if( bakiye-tutar < 0){
               System.out.print("Yeterli bakiye yok.Bakiyeniz: "+bakiye);
@@ -370,7 +370,7 @@ public class b{
        else if(islem.equals("3")){
          System.out.print("Yatırılmak istenilen tutar: ");
          int tutar=scanner.nextInt();
-         sc.nextLine();
+         scanner.nextLine();
  
          bakiye+=tutar;
          System.out.print("Yeni bakiyeniz: "+bakiye);
@@ -380,10 +380,82 @@ public class b{
   }
  }
 }
-// ------------------------------------------------------------------
+------------------------------------------------------------------
 // Armstrong sayı bulma.
-  public class b{
-   public static void main(String[] args){
-     
- }
+ import java.util.Scanner;
+public class b {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sayi girin:");
+        int sayi=scanner.nextInt();
+        int basamak = String.valueOf(sayi).length();
+        int toplam=0;
+        int gecici=sayi;
+
+        do{
+            int deger = gecici % 10;
+            gecici /= 10;
+            toplam += Math.pow(deger,basamak);
+
+        }while(gecici > 0);
+
+        if(sayi==toplam){
+            System.out.print("Armstrong sayisidir.");
+        }
+        else{System.out.print("Armstrong sayisi degildir.");
+        }
+    }
 }
+------------------------------------------------------------
+// İç içe for döngüsü ile çarpım tablosu olusturma.
+public class b {
+    public static void main(String[] args){
+
+     for (int i=1; i<=10; i++) {
+         System.out.println("********");
+        for(int j=1; j<=10; j++){
+            System.out.println(i+"x" +j +"="+(i*j));
+        }
+     }
+    }
+}
+------------------------------------------------------
+// While ile kullanıcı girişi.
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class b {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int girisHakki = 3;
+        String kullaniciAdi = "SGOKTAS";
+        String parola = "1234";
+
+        while (true) {
+            System.out.println("Kullanıcı adı girin: ");
+            String kullanici = scanner.nextLine();
+            System.out.println("Parola girin: ");
+            String password = scanner.nextLine();
+
+            if (parola.equals(password) && kullaniciAdi.equals(kullanici)) { // İki stringin eşleşme kontrolü
+                System.out.println("Giris Basarılı " + kullaniciAdi);
+                break;
+            } else if (!parola.equals(password) && kullaniciAdi.equals(kullanici)) {
+                System.out.println("Sifre/Kullanici Adi hatali...");
+                girisHakki -= 1 ;
+
+            } else if (parola.equals(password) && !kullaniciAdi.equals(kullanici)) {
+                System.out.println("Sifre/Kullanici Adi hatali...");
+                girisHakki -= 1 ;
+            } else {
+                System.out.println("Sifre ve Kullanici Adi hatali...");
+                girisHakki -= 1;
+            }
+            if (girisHakki==0){
+                System.out.println("Giris hakkiniz dolmustur...");
+                break;
+            }
+        }
+    }
+}
+------------------------------------------------------------------
